fa65ff4c73ba409c1832ed67a81aff394d3cbc37
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2021-06-21 04:54:24 -0300

    Adding showInventoryOnHand - closes #20

--- src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -5,20 +5,74 @@ import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import guru.springframework.msscbeerservice.web.controller.NotFoundException;
 import guru.springframework.msscbeerservice.web.mappers.BeerMapper;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerPagedList;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
-@Service("beerService")
+@Service
 public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
     @Override
-    public  BeerDto getById(UUID beerId) {
-       return beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+    public BeerPagedList listBeer(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
+       BeerPagedList beerPagedList;
+       Page<Beer> beerPage;
+
+       if(!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+           // search both
+           beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
+       } else if(!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
+           //search beer_service name
+           beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
+        }else if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+           //search beer_service style
+           beerPage = beerRepository.findAllByBeerStyle(beerStyle, pageRequest);
+       } else {
+           beerPage = beerRepository.findAll(pageRequest);
+       }
+
+       if(showInventoryOnHand) {
+           beerPagedList = new BeerPagedList(beerPage
+                   .getContent()
+                   .stream()
+                   .map(beerMapper::beerToBeerDtoWithInventory)
+                   .collect(Collectors.toList()),
+                   PageRequest
+                           .of(beerPage.getPageable().getPageNumber(),
+                                   beerPage.getPageable().getPageSize()),
+                   beerPage.getTotalElements());
+       }else {
+           beerPagedList = new BeerPagedList(beerPage
+                   .getContent()
+                   .stream()
+                   .map(beerMapper::beerToBeerDto)
+                   .collect(Collectors.toList()),
+                   PageRequest
+                           .of(beerPage.getPageable().getPageNumber(),
+                                   beerPage.getPageable().getPageSize()),
+                   beerPage.getTotalElements());
+       }
+       return beerPagedList;
+    }
+
+    @Override
+    public  BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+      if(showInventoryOnHand){
+          return beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+      }else{
+          return beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+      }
+
+
     }
 
     @Override
